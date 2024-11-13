ab1cc44e033db51051c93bbac9d987a6da70fe03
Jacob Schulte <schul2jt@mail.uc.edu>
2024-11-13 11:04:22 -0500

    main for api

--- Trout_Assignment10/mainPackage/main.py
+++ Trout_Assignment10/mainPackage/main.py
@@ -1,1 +1,18 @@
 
+
+# File: main.py
+from dataPackage.bdsAPI import BEAApi
+
+if __name__ == "__main__":
+
+    # Instantiate the BEAApi class
+    api = BEAApi()
+
+    # Get the data from the API
+    data = api.get_request_url()
+
+    # Print the value for 'Computers and Peripheral Equipment' fixed assets in 2023
+    api.print_computers_peripheral_equipment_2023(data)
+    
+    #save the JSON file to a CSV file
+    api.save_json_to_csv(data)
